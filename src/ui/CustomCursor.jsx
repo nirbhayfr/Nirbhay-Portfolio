13ec2fc79f2b973151c4d355736c671c7824af21
nirbhayfr <nirbhaychauhan0306@gmail.com>
2025-05-15 16:13:22 +0530

    Bug Fixes

--- src/ui/CustomCursor.jsx
+++ src/ui/CustomCursor.jsx
@@ -1,9 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function CustomCursor() {
   //   const dot = useRef();
   const dotOutline = useRef();
-  console.log(dotOutline);
   window.addEventListener("mousemove", function (e) {
     const posX = e.clientX;
     const posY = e.clientY;
@@ -20,9 +19,13 @@ function CustomCursor() {
     );
   });
 
+  const [width] = useState(window.innerWidth);
+
+  if (width > 768) return null;
+
   return (
     <div
-      className="border-secondary pointer-events-none fixed top-0 left-0 z-100 h-6 w-6 -translate-x-1/2 -translate-y-1/2 rounded-full border-2"
+      className="border-secondary pointer-events-none fixed top-0 left-0 z-100 h-6 w-6 -translate-x-1/2 -translate-y-1/2 rounded-full border-2 max-md:hidden"
       ref={dotOutline}
     ></div>
   );
